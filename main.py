baa42911b2d426fd3ddf6d602748c89a7e86c591
oreocookie1 <82494923+oreocookie1@users.noreply.github.com>
2023-01-11 15:52:34 +0900

    230111

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ noise = init.normal_(torch.FloatTensor(num_data,1), std=1)
 y= 2*x +3
 y_noise = y + noise
 model = nn.Linear(1,1)
-
 optimizer = optim.SGD(model.parameters(), lr=0.01)
 
 label = y_noise
@@ -18,6 +17,26 @@ for i in range(num_epoch) :
     optimizer.zero_grad()
     output =model(x)
 
-print(hello world)
-#loss_func = nn.L1Loss(y-y_noise)
+print("hello world")
+#loss_func = nn.L1Loss(y-y_noise)j
+
+
+##### dkssudgktpwy ... ...
+num_data=500
+num_epoch = 1000
+noise = init.normal_(torch.FloatTensor(num_data,1), std=1)
+x = init.uniform_(torch.FloatTensor(num_data ,1),-15,15)
+y = (x**2) + 3
+y_noise = y + noise
+
+model = nn.Sequential(nn.Linear(1,6),
+                      nn.ReLU(),
+                      nn.Linear(6,10),
+                      nn.ReLU(),
+                      nn.Linear(10,6),
+                      nn.ReLU(),
+                      nn.Linear(6,1),)
+
+loss_func = nn.L1Loss()
+optimizer = optim.SGD(model.parameters(),lr=0.0002)
 
